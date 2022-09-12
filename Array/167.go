a35e8fb52f3102c09f519a90090a182c17152351
gooohlan <gooohlan@qq.com>
2022-09-12 21:28:12 +0800

    feat:167 283 :saprkles:

---
+++ Array/167.go
@@ -0,0 +1,19 @@
+package Array
+
+// 定义左右指针分别执行数组第一个元素和最后一个元素
+// 根据题意，数组是从小到大排列的，在左指针小于右指针的前提下，用他们的和依次去与target进行比较
+// 比target大时，右指针往左，和变小
+// 比target小时，左指针往右，和变大
+
+func twoSum(numbers []int, target int) []int {
+	for i, j := 0, len(numbers)-1; i < j; {
+		if numbers[i]+numbers[j] < target {
+			i++
+		} else if numbers[i]+numbers[j] > target {
+			j--
+		} else {
+			return []int{i + 1, j + 1}
+		}
+	}
+	return nil
+}
---
+++ Array/283.go
@@ -0,0 +1,16 @@
+package Array
+
+import "fmt"
+
+// 快指针fast向前遍历, 遇到非0值就与慢指针slow所在值进行交换,快指针遍历结束,所有的0都处于数组后半段
+// 此题与26和27的思路类似,可以看做是27题的移除元素0的升级版,把移除变成了交换位置
+func moveZeroes(nums []int) {
+	slow := 0
+	for fast := 0; fast < len(nums); fast++ {
+		if nums[fast] != 0 {
+			nums[fast], nums[slow] = nums[slow], nums[fast]
+			slow++
+		}
+	}
+	fmt.Println(slow)
+}
