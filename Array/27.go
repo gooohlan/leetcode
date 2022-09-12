196b3d9b98d3361be8bb9d031c5ab9f8353df110
gooohlan <gooohlan@qq.com>
2022-09-12 18:53:40 +0800

    feat:27 :saprkles:

---
+++ Array/27.go
@@ -0,0 +1,15 @@
+package Array
+
+// 依旧采用快慢指针的方式
+// 如果fast遇到只为val的元素,则直接跳过,否则赋值给slow所在值,slow前进一步
+func removeElement(nums []int, val int) int {
+	slow := 0
+	for fast := 0; fast < len(nums); fast++ {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	// 因为先复制后前进,所以返回值不+1
+	return slow
+}
--- Array/array_test.go
+++ Array/array_test.go
@@ -8,3 +8,9 @@ import (
 func TestRemoveDuplicates(t *testing.T) {
 	fmt.Println(removeDuplicates([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}))
 }
+
+func TestRemoveElement(t *testing.T) {
+	arr := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	val := 2
+	fmt.Println(removeElement(arr, val))
+}
