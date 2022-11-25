4b7918dee6d6443b168b88c4970d64093b65d8cd
gooohlan <gooohlan@qq.com>
2022-11-25 18:19:27 +0800

    feat:130-bfs

--- Graph/130.go
+++ Graph/130.go
@@ -39,3 +39,61 @@ func solveDFS(board [][]byte) {
 		}
 	}
 }
+
+func solveBFS(board [][]byte) {
+	var ( // x,y分别代表当前元素的4个方向
+		x = [4]int{1, -1, 0, 0}
+		y = [4]int{0, 0, 1, -1}
+	)
+	if len(board) == 0 || len(board[0]) == 0 {
+		return
+	}
+	n, m := len(board), len(board[0])
+	q := [][]int{}
+	// 先标记边界上的'O'
+	for i := 0; i < n; i++ { // 标记第一行和最后一行
+		if board[i][0] == 'O' {
+			q = append(q, []int{i, 0})
+			board[i][0] = '#'
+		}
+		if board[i][m-1] == 'O' {
+			q = append(q, []int{i, m - 1})
+			board[i][m-1] = '#'
+		}
+	}
+	for i := 1; i < m-1; i++ { //  标记第一列和最后一列
+		if board[0][i] == 'O' {
+			q = append(q, []int{0, i})
+			board[0][i] = '#'
+		}
+		if board[n-1][i] == 'O' {
+			q = append(q, []int{n - 1, i})
+			board[n-1][i] = '#'
+		}
+	}
+
+	// 标记与边界相邻的
+	for len(q) > 0 {
+		cur := q[0]
+		q = q[1:]
+		for i := 0; i < 4; i++ {
+			mx, my := cur[0]+x[i], cur[1]+y[i]
+			if mx < 0 || mx >= n || my < 0 || my >= m || board[mx][my] != 'O' {
+				continue
+			}
+			q = append(q, []int{mx, my})
+			board[mx][my] = '#'
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if board[i][j] == 'O' { // 未被标记,变成X
+				board[i][j] = 'X'
+			}
+			if board[i][j] == '#' { // 被标记无需改变,变回'O'
+				board[i][j] = 'O'
+			}
+		}
+	}
+}
