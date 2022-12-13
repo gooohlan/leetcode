cd1fcf0ab14ee7b8d2983a7785bb971533185697
gooohlan <gooohlan@qq.com>
2022-12-13 20:58:42 +0800

    feat:355

---
+++ DataStructure/355.go
@@ -0,0 +1,125 @@
+package DataStructure
+
+import "container/heap"
+
+type Twitter struct {
+    users map[int]*user
+    time  int
+}
+
+type user struct {
+    id        int
+    followed  map[int]*user
+    tweetList *tweet
+}
+
+type tweet struct {
+    id   int
+    time int
+    next *tweet
+}
+
+type tweetHeap []*tweet
+
+func (h tweetHeap) Len() int { return len(h) }
+
+func (h tweetHeap) Less(i, j int) bool { return h[i].time > h[j].time }
+
+func (h tweetHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+
+func (h *tweetHeap) Push(x interface{}) { *h = append(*h, x.(*tweet)) }
+
+func (h *tweetHeap) Pop() (v interface{}) {
+    old := *h
+    *h, v = old[:len(old)-1], old[len(old)-1]
+    return
+}
+
+func Constructor355() Twitter {
+    return Twitter{
+        users: make(map[int]*user),
+    }
+}
+
+func (t *Twitter) PostTweet(userId int, tweetId int) {
+    text := tweet{
+        id:   tweetId,
+        time: t.getTime(),
+    }
+    user := t.getUser(userId)
+    text.next = user.tweetList
+    user.tweetList = &text
+}
+
+func (t *Twitter) GetNewsFeed(userId int) []int {
+    user := t.getUser(userId)
+    texts := make([]*tweet, 0, len(user.followed)+1)
+    if user.tweetList != nil {
+        texts = append(texts, user.tweetList)
+    }
+    
+    for _, u := range user.followed {
+        if u.tweetList != nil {
+            texts = append(texts, u.tweetList)
+        }
+    }
+    
+    res := make([]int, 0, 10)
+    textHeap := tweetHeap(texts)
+    h := &textHeap
+    heap.Init(h)
+    
+    for len(res) < 10 && h.Len() > 0 {
+        text := heap.Pop(h).(*tweet)
+        res = append(res, text.id)
+        if text.next != nil {
+            heap.Push(h, text.next)
+        }
+    }
+    return res
+}
+
+func (t *Twitter) Follow(followerId int, followeeId int) {
+    if followerId == followeeId {
+        return
+    }
+    
+    user1 := t.getUser(followerId)
+    user2 := t.getUser(followeeId)
+    user1.followed[followerId] = user2
+}
+
+func (t *Twitter) Unfollow(followerId int, followeeId int) {
+    if followerId == followeeId {
+        return
+    }
+    user := t.getUser(followerId)
+    _ = t.getUser(followeeId)
+    delete(user.followed, followeeId)
+}
+
+func (t *Twitter) getTime() int {
+    t.time++
+    return t.time
+}
+
+func (t *Twitter) getUser(id int) *user {
+    u, ok := t.users[id]
+    if !ok {
+        u = &user{
+            id:       id,
+            followed: make(map[int]*user),
+        }
+        t.users[id] = u
+    }
+    return u
+}
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.PostTweet(userId,tweetId);
+ * param_2 := obj.GetNewsFeed(userId);
+ * obj.Follow(followerId,followeeId);
+ * obj.Unfollow(followerId,followeeId);
+ */
--- DataStructure/data_structure_test.go
+++ DataStructure/data_structure_test.go
@@ -1,113 +1,117 @@
 package DataStructure
 
 import (
-	"fmt"
-	"testing"
+    "fmt"
+    "testing"
 )
 
 func TestLRU(t *testing.T) {
-	constructor := Constructor2(3)
-	constructor.Put(1, 1)
-	constructor.Put(2, 2)
-	constructor.Put(2, 2222)
-	constructor.Put(3, 3)
-	constructor.Put(4, 4)
-	constructor.Get(2)
-	constructor.Put(5, 5)
+    constructor := Constructor2(3)
+    constructor.Put(1, 1)
+    constructor.Put(2, 2)
+    constructor.Put(2, 2222)
+    constructor.Put(3, 3)
+    constructor.Put(4, 4)
+    constructor.Get(2)
+    constructor.Put(5, 5)
 }
 
 func TestLFU(t *testing.T) {
-	constructor := Constructor460(0)
-	constructor.Put(0, 0)
-	constructor.Put(2, 2)
-	constructor.Put(1, 1111)
-	constructor.Get(2)
-	constructor.Put(3, 3)
+    constructor := Constructor460(0)
+    constructor.Put(0, 0)
+    constructor.Put(2, 2)
+    constructor.Put(1, 1111)
+    constructor.Get(2)
+    constructor.Put(3, 3)
 }
 
 func Test208(t *testing.T) {
-	trie := Constructor208()
-	trie.Insert("apple")
-	fmt.Println(trie.Search("app"))
-	fmt.Println(trie.StartsWith("app"))
-	trie.Insert("app")
-	fmt.Println(trie.Search("app"))
-	fmt.Println(trie.ShortestPrefixOf("adpbbccs"))
+    trie := Constructor208()
+    trie.Insert("apple")
+    fmt.Println(trie.Search("app"))
+    fmt.Println(trie.StartsWith("app"))
+    trie.Insert("app")
+    fmt.Println(trie.Search("app"))
+    fmt.Println(trie.ShortestPrefixOf("adpbbccs"))
 }
 
 func Test648(t *testing.T) {
-	dictionary := []string{
-		"a", "b", "c",
-	}
-	fmt.Println(replaceWords(dictionary, "aadsfasf absbs bbab cadsfafs"))
-	fmt.Println(replaceWords2(dictionary, "aadsfasf absbs bbab cadsfafs"))
+    dictionary := []string{
+        "a", "b", "c",
+    }
+    fmt.Println(replaceWords(dictionary, "aadsfasf absbs bbab cadsfafs"))
+    fmt.Println(replaceWords2(dictionary, "aadsfasf absbs bbab cadsfafs"))
 }
 
 func Test211(t *testing.T) {
-	wordDictionary := Constructor211()
-	wordDictionary.AddWord("bad")
-	wordDictionary.AddWord("dad")
-	wordDictionary.AddWord("mad")
-	fmt.Println(wordDictionary.Search("pad"))
-	fmt.Println(wordDictionary.Search("bad"))
-	fmt.Println(wordDictionary.Search(".ad"))
-	fmt.Println(wordDictionary.Search("b.."))
+    wordDictionary := Constructor211()
+    wordDictionary.AddWord("bad")
+    wordDictionary.AddWord("dad")
+    wordDictionary.AddWord("mad")
+    fmt.Println(wordDictionary.Search("pad"))
+    fmt.Println(wordDictionary.Search("bad"))
+    fmt.Println(wordDictionary.Search(".ad"))
+    fmt.Println(wordDictionary.Search("b.."))
 }
 
 func Test1804(t *testing.T) {
-	trie := Constructor1804()
-	trie.Insert("abbce")
-	trie.Insert("abbce")
-	trie.Insert("abb")
-	fmt.Println(trie.CountWordsEqualTo("abbce"))
-	fmt.Println(trie.CountWordsStartingWith("abb"))
-	trie.Erase("abbce")
-	fmt.Println(trie.CountWordsEqualTo("abbce"))
-	fmt.Println(trie.CountWordsStartingWith("abb"))
-	trie.Erase("abb")
-	fmt.Println(trie.CountWordsEqualTo("abbce"))
-	fmt.Println(trie.CountWordsStartingWith("abb"))
-	trie.Erase("abbce")
-	fmt.Println(trie.CountWordsEqualTo("abbce"))
-	fmt.Println(trie.CountWordsStartingWith("abb"))
+    trie := Constructor1804()
+    trie.Insert("abbce")
+    trie.Insert("abbce")
+    trie.Insert("abb")
+    fmt.Println(trie.CountWordsEqualTo("abbce"))
+    fmt.Println(trie.CountWordsStartingWith("abb"))
+    trie.Erase("abbce")
+    fmt.Println(trie.CountWordsEqualTo("abbce"))
+    fmt.Println(trie.CountWordsStartingWith("abb"))
+    trie.Erase("abb")
+    fmt.Println(trie.CountWordsEqualTo("abbce"))
+    fmt.Println(trie.CountWordsStartingWith("abb"))
+    trie.Erase("abbce")
+    fmt.Println(trie.CountWordsEqualTo("abbce"))
+    fmt.Println(trie.CountWordsStartingWith("abb"))
 }
 
 func Test677(t *testing.T) {
-	mapSum := Constructor677()
-	mapSum.Insert("apple", 3)
-	fmt.Println(mapSum.Sum("ap"))
-	mapSum.Insert("app", 2)
-	mapSum.Insert("apple", 2)
-	fmt.Println(mapSum.Sum("ap"))
+    mapSum := Constructor677()
+    mapSum.Insert("apple", 3)
+    fmt.Println(mapSum.Sum("ap"))
+    mapSum.Insert("app", 2)
+    mapSum.Insert("apple", 2)
+    fmt.Println(mapSum.Sum("ap"))
 }
 
 func Test295(t *testing.T) {
-	this := Constructor295()
-	this.AddNum(1)
-	this.AddNum(2)
-	this.AddNum(3)
-	this.AddNum(4)
-	this.AddNum(5)
-	fmt.Println(this.FindMedian())
+    this := Constructor295()
+    this.AddNum(1)
+    this.AddNum(2)
+    this.AddNum(3)
+    this.AddNum(4)
+    this.AddNum(5)
+    fmt.Println(this.FindMedian())
 }
 
 func Test496(t *testing.T) {
-	nums1 := []int{4, 1, 2}
-	nums2 := []int{1, 3, 4, 2}
-	fmt.Println(nextGreaterElement(nums1, nums2))
+    nums1 := []int{4, 1, 2}
+    nums2 := []int{1, 3, 4, 2}
+    fmt.Println(nextGreaterElement(nums1, nums2))
 }
 
 func Test739(t *testing.T) {
-	nums := []int{73, 74, 75, 71, 69, 76}
-	fmt.Println(dailyTemperatures2(nums))
+    nums := []int{73, 74, 75, 71, 69, 76}
+    fmt.Println(dailyTemperatures2(nums))
 }
 
 func Test503(t *testing.T) {
-	nums := []int{1, 1, 1, 1}
-	fmt.Println(nextGreaterElements(nums))
+    nums := []int{1, 1, 1, 1}
+    fmt.Println(nextGreaterElements(nums))
 }
 
 func Test239(t *testing.T) {
-	nums := []int{1, 3, -1, -3, 5, 3, 6, 7}
-	fmt.Println(maxSlidingWindow(nums, 3))
+    nums := []int{1, 3, -1, -3, 5, 3, 6, 7}
+    fmt.Println(maxSlidingWindow(nums, 3))
+}
+
+func Test355(t *testing.T) {
+    
 }
