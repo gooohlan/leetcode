654a08426eb5568c9f220b8ee2ee5e037cc1aa99
gooohlan <gooohlan@qq.com>
2022-12-18 13:44:52 +0800

    feat:6267

--- WeeklyContest/6267.go
+++ WeeklyContest/6267.go
@@ -1,46 +1,53 @@
 package WeeklyContest
 
+type pair struct {
+    x, y int
+}
+
 func isPossible(n int, edges [][]int) bool {
-    var (
-        graph    = make([][]int, n+1)
-        indegree = make([]int, n+1) // 入度数
-    )
+    has := make(map[pair]bool)
+    deg := make([]int, n+1)
     
-    for _, item := range edges {
-        graph[item[1]] = append(graph[item[1]], item[0])
-        graph[item[0]] = append(graph[item[0]], item[1])
-        indegree[item[0]]++
+    for _, edge := range edges {
+        x, y := edge[0], edge[1]
+        has[pair{x, y}] = true
+        has[pair{y, x}] = true
+        deg[x]++
+        deg[y]++
     }
-    
-    oddEdge := make(map[int]bool)
-    for i := 1; i < n+1; i++ {
-        if indegree[i]%2 != 0 {
-            oddEdge[i] = true
+    odd := []int{} // 记录出度为奇数的节点
+    for i, d := range deg {
+        if d%2 != 0 {
+            odd = append(odd, i)
         }
     }
-    if len(oddEdge) > 4 {
-        return false
+    
+    // 奇数点数为0,已经符合要求
+    if len(odd) == 0 {
+        return true
     }
-    res := 0
-    for v, _ := range oddEdge {
-        i := 1
-        for _, cur := range graph[v] {
-            for ; i < cur; i++ {
-                if oddEdge[i] && i != v {
-                    res++
-                    delete(oddEdge, v)
-                    delete(oddEdge, i)
-                    break
-                }
-            }
-            if !oddEdge[v] {
-                break
-            }
-            i++
+    // 奇数点数为2
+    if len(odd) == 2 {
+        x, y := odd[0], odd[1]
+        if !has[pair{x, y}] { // 两个奇数点未相连,直接相连即可
+            return true
         }
-    }
-    if res > 2 || len(oddEdge) != 0 {
+        // 找到点i, i满足不与x,y有边,连接i->x,i->y即可
+        for i := 1; i < n; i++ {
+            if i != x && i != y && !has[pair{i, x}] && !has[pair{i, y}] {
+                return true
+            }
+        }
         return false
     }
-    return true
+    
+    // 4个奇数点,分别记作a,b,c,d, 存在两两不相连,连接即可
+    if len(odd) == 4 {
+        a, b, c, d := odd[0], odd[1], odd[2], odd[3]
+        return !has[pair{a, b}] && !has[pair{c, d}] ||
+            !has[pair{a, c}] && !has[pair{b, d}] ||
+            !has[pair{a, d}] && !has[pair{c, b}]
+    }
+    
+    return false
 }
