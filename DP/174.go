d85ffb6ab94b45459fe25ad05dec76f1cf14e34e
gooohlan <gooohlan@qq.com>
2023-01-05 08:53:57 +0800

    feat:174初始化

---
+++ DP/174.go
@@ -0,0 +1,9 @@
+package DP
+
+func calculateMinimumHP(dungeon [][]int) int {
+    var dfs func(i, j int) int // 从i,j到右下角最低消耗血量
+    
+    dfs = func(i, j int) int {
+        
+    }
+}
--- DP/dp_test.go
+++ DP/dp_test.go
@@ -140,3 +140,13 @@ func Test64(t *testing.T) {
     fmt.Println(minPathSumDP(grid))
     fmt.Println(minPathSumDP2(grid))
 }
+
+func Test174(t *testing.T) {
+    dungeon := [][]int{
+        []int{-2, -3, 3},
+        []int{-5, -10, 1},
+        []int{10, 30, 5},
+    }
+    
+    fmt.Println(calculateMinimumHP(dungeon))
+}
