88d3627767f23353bcb9678a82eeadf78c914978
gooohlan <gooohlan@qq.com>
2023-01-11 15:33:26 +0800

    feat:6276

---
+++ WeeklyContest/6276.go
@@ -0,0 +1,31 @@
+package WeeklyContest
+
+import (
+    "strings"
+)
+
+const mod int = 1e9 + 7
+func countAnagrams(s string) int {
+    ans, mul := 1, 1
+    for _, s := range strings.Split(s, " ") {
+        cnt := [26]int{}
+        for i, c := range s {
+            cnt[c-'a']++
+            mul = mul * cnt[c-'a'] %mod
+            ans = ans * (i+1) %mod
+        }
+    }
+    
+    return ans * pow(mul, mod-2) % mod
+}
+
+func pow(x, n int) int {
+    res := 1
+    for ; n > 0; n >>= 1 {
+        if n&1 > 0 {
+            res = res * x % mod
+        }
+        x = x * x % mod
+    }
+    return res
+}
