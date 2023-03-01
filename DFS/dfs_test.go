89bb56df94617acb1c936743e3a8c0516a55f614
gooohlan <gooohlan@qq.com>
2023-03-01 08:10:57 +0800

    feat:1254

--- DFS/dfs_test.go
+++ DFS/dfs_test.go
@@ -14,3 +14,10 @@ func Test200(t *testing.T) {
     }
     fmt.Println(numIslands(grid))
 }
+
+func Test1254(t *testing.T) {
+    grid := [][]int{
+        {0, 0, 1, 0, 0}, {0, 1, 0, 1, 0}, {0, 1, 1, 1, 0},
+    }
+    fmt.Println(closedIsland(grid))
+}
