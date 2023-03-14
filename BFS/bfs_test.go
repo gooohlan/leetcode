78e983a12f156f5f4bc1be4bff6a9aa840102b76
gooohlan <gooohlan@qq.com>
2023-03-14 22:21:55 +0800

    feat:384

--- BFS/bfs_test.go
+++ BFS/bfs_test.go
@@ -1,31 +1,31 @@
 package BFS
 
 import (
-	"fmt"
-	"testing"
+    "fmt"
+    "testing"
 )
 
 func TestMinDepth(t *testing.T) {
-	node := &TreeNode{
-		1,
-		&TreeNode{2, &TreeNode{4, nil, nil}, nil},
-		&TreeNode{3, nil, &TreeNode{5, nil, nil}},
-	}
-	depth := minDepth(node)
-	fmt.Println(depth)
+    node := &TreeNode{
+        1,
+        &TreeNode{2, &TreeNode{4, nil, nil}, nil},
+        &TreeNode{3, nil, &TreeNode{5, nil, nil}},
+    }
+    depth := minDepth(node)
+    fmt.Println(depth)
 }
 
 func TestOpenLock(t *testing.T) {
-	deadends := []string{"0201", "0101", "0102", "1212", "2002"}
-	// fmt.Println(openLock(deadends, "0202"))
-	fmt.Println(openLock2(deadends, "0202"))
+    deadends := []string{"0201", "0101", "0102", "1212", "2002"}
+    // fmt.Println(openLock(deadends, "0202"))
+    fmt.Println(openLock2(deadends, "0202"))
 }
 
 func TestSlidingPuzzle(t *testing.T) {
-	board := [][]int{
-		[]int{4, 1, 2},
-		[]int{5, 0, 3},
-	}
-	// fmt.Println(slidingPuzzle(board))
-	fmt.Println(slidingPuzzle2(board))
+    board := [][]int{
+        []int{4, 1, 2},
+        []int{5, 0, 3},
+    }
+    fmt.Println(slidingPuzzle(board))
+    fmt.Println(slidingPuzzle2(board))
 }
