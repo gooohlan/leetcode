815dd15f25bebcbde391c5b90e2447816a247b51
gooohlan <gooohlan@qq.com>
2023-05-30 08:55:10 +0800

    feat: 2710

---
+++ WeeklyContest/2710.go
@@ -0,0 +1,12 @@
+package WeeklyContest
+
+import "strings"
+
+func removeTrailingZeros(num string) string {
+    b := []byte(num)
+    i := len(b)
+    for ; i > 0 && num[i] == '0'; i-- {
+    }
+    return string(b[:i+1])
+    return strings.TrimRight(num, "0")
+}
