2ab0e0b0fb343ebaab9c9dd9edeac01390380706
gooohlan <gooohlan@qq.com>
2023-06-04 10:02:45 +0800

    docs: 2710

--- WeeklyContest/2710.go
+++ WeeklyContest/2710.go
@@ -8,5 +8,6 @@ func removeTrailingZeros(num string) string {
     for ; i > 0 && num[i] == '0'; i-- {
     }
     return string(b[:i+1])
-    return strings.TrimRight(num, "0")
+    
+    return strings.TrimRight(num, "0") // 库函数写法
 }
