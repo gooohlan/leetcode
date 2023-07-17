168a4dd12e68c638998474cbaef82af10efd4a42
gooohlan <gooohlan@qq.com>
2023-07-17 08:48:23 +0800

    feat:2749

---
+++ WeeklyContest/2749.go
@@ -0,0 +1,12 @@
+package WeeklyContest
+
+import "math/bits"
+
+func makeTheIntegerZero(num1 int, num2 int) int {
+    for k := 1; k <= num1-num2*k; k++ {
+        if k >= bits.OnesCount(uint(num1-num2*k)) {
+            return k
+        }
+    }
+    return -1
+}
